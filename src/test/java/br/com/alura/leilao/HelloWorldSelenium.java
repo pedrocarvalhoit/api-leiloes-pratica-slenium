0c1942ff32280f34c7e1b703f87a1e605c4d7a42
Pedro Duarte <pedrohdc@yahoo.com.br>
2022-11-29 14:58:16 +0000

    Initial commit

---
+++ src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
@@ -0,0 +1,17 @@
+package br.com.alura.leilao;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorldSelenium {
+
+    @Test
+    public void Hello(){
+        //Iniciando navegador
+        WebDriver browser = new ChromeDriver();
+        browser.navigate().to("http://localhost:8080/leiloes");
+        browser.quit();
+    }
+
+}
